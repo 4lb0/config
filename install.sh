38e69af419d68e647eb76234f26a247e2dd439cc
Rodrigo “Albo” Arce <albo@pragmore.com>
2023-06-10 19:34:36 -0300

    Add tmux config and plugins

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ ln -sf $CONFIG_PATH/init.vim ~/.config/nvim/init.vim
 ln -sf $CONFIG_PATH/.zshrc ~/.zshrc
 ln -sf $CONFIG_PATH/.gitconfig ~/.gitconfig
 ln -sf $CONFIG_PATH/skeletons ~/.vim/skeletons
+ln -sf $CONFIG_PATH/.tmux.conf ~/.tmux.conf
