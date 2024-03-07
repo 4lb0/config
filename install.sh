4be2ac9aa07950cee5c394b4ef5fb7e610e289b5
Rodrigo Arce <rsarce@gmail.com>
2024-03-07 15:52:55 -0300

    Add SSH config

--- install.sh
+++ install.sh
@@ -5,3 +5,4 @@ ln -sf $CONFIG_PATH/.zshrc ~/.zshrc
 ln -sf $CONFIG_PATH/.gitconfig ~/.gitconfig
 ln -sf $CONFIG_PATH/skeletons ~/.vim/skeletons
 ln -sf $CONFIG_PATH/.tmux.conf ~/.tmux.conf
+ln -sf $CONFIG_PATH/ssh_config ~/.ssh/config
